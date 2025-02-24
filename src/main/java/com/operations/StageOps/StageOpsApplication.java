67e79a3845f243fb6852a5685ea36f0350316272
Monkey-D-MC1 <mahfuzc542@gmail.com>
2025-02-24 13:31:35 +0000

    Initial commit

---
+++ src/main/java/com/operations/StageOps/StageOpsApplication.java
@@ -0,0 +1,13 @@
+package com.operations.StageOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageOpsApplication.class, args);
+	}
+
+}
